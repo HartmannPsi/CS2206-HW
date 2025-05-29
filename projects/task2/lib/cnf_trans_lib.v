9be51427f814e124220cfba2cad5af93d8c8fab0
HartmannPsi <hartmann_psi@sjtu.edu.cn>
2025-05-29 19:52:20 +0800

    feat: modified cnf_list_cell, prop2cnf, prop_cnt; modified path mapping in Makefile

--- projects/task2/lib/cnf_trans_lib.v
+++ projects/task2/lib/cnf_trans_lib.v
@@ -22,11 +22,11 @@ Local Open Scope sac.
 
 From SimpleC.EE Require Import super_poly_sll2.
 From SimpleC.EE Require Import malloc.
+From SimpleC.EE Require Import smt_lang_lib.
 
-Inductive cnf_list_cell : Type :=
-  | CNFCell (size: Z) (clause: list Z): cnf_list_cell.
+Definition cnf_list_cell := list Z.
 
-Definition cnf_list : Type := list cnf_list_cell.
+Definition cnf_list : Type := list (list Z).
 
 (* Inductive PreData : Type :=
   | PD (cnf_res: cnf_list) (prop_cnt: Z) (clause_cnt: Z): PreData. *)
@@ -36,21 +36,14 @@ Definition init_int_array (x: addr) (size: Z): Assertion :=
   store_int_array x size (all_zero_list size).
 
 Definition store_clause (x: addr) (clause: list Z): Assertion :=
-  [| Zlength clause <= 3 |] && [| Forall (fun z => z <> 0) clause |] &&
+ [| Zlength clause <= 3 |] && [| Forall (fun z => z <> 0) clause |] &&
   store_int_array x 3%Z (clause ++ all_zero_list (3 - Zlength clause)).
 
-Definition store_cnf_list_cell (x: addr) (c: cnf_list_cell): Assertion :=
-  match c with
-    | CNFCell size clause => [| x <> NULL |] && [| Zlength clause = size |] &&
-                             EX y: addr,
-                              &(x # "cnf_list" ->ₛ "size") # Int |-> size **
-                              &(x # "cnf_list" ->ₛ "clause") # Ptr |-> y **
-                              store_clause y clause
-  end.
-
-(* Definition link_cnf_list_cell (x y: addr): Assertion :=
-  [| x <> NULL |] &&
-  &(x # "cnf_list" ->ₛ "next") # Ptr |-> y. *)
+Definition store_cnf_list_cell (x: addr) (clause: list Z): Assertion :=
+ [| x <> NULL |] && EX y: addr,
+  &(x # "cnf_list" ->ₛ "size") # Int |-> 3%Z **
+  &(x # "cnf_list" ->ₛ "clause") # Ptr |-> y **
+  store_clause y clause.
 
 Module cnf_list_store_lists.
 
@@ -67,12 +60,8 @@ End cnf_list_store_lists.
 
 Import cnf_list_store_lists.
 
-(* Definition zlength {A: Type} (l: list A) : Z :=
-  Z.of_nat (List.length l). *)
-
-Definition Z_union (l1 l2 : list Z) : list Z :=
+(* Definition Z_union (l1 l2 : list Z) : list Z :=
   nodup Z.eq_dec (l1 ++ l2).
-
 
 Fixpoint all_vars (l: cnf_list): list Z :=
   match l with
@@ -81,69 +70,79 @@ Fixpoint all_vars (l: cnf_list): list Z :=
   end.
 
 Definition all_vars_cnt (l: cnf_list): Z :=
-  Zlength (all_vars l).
+  Zlength (all_vars l). *)
 
-(* Compute Z_union [1; 2; 3] [2; 4; 5; 1]. *)
-  (* = [1; 2; 3; 4; 5] : list Z *)
+Fixpoint max_element_alt (l: list Z): Z :=
+  match l with
+    | nil => 0%Z
+    | h :: t => Z.max h (max_element_alt t)
+  end.
+
+Fixpoint max_element (l: cnf_list): Z :=
+  match l with
+    | nil => 0%Z
+    | clause :: t => Z.max (max_element_alt clause) (max_element t)
+  end.
 
 Definition store_predata (x: addr) (cnf_res: cnf_list) (prop_cnt clause_cnt: Z): Assertion :=
-  [| x <> NULL |] && [| Zlength cnf_res = clause_cnt |] &&
-  [| all_vars_cnt cnf_res = prop_cnt |] &&
+ [| x <> NULL |] && [| Zlength cnf_res = clause_cnt |] &&
+ [| max_element cnf_res <= prop_cnt |] &&
   EX y: addr,
     &(x # "PreData" ->ₛ "cnf_res") # Ptr |-> y **
     &(x # "PreData" ->ₛ "prop_cnt") # Int |-> prop_cnt **
     &(x # "PreData" ->ₛ "clause_cnt") # Int |-> clause_cnt **
     sll_cnf_list y cnf_res.
 
-Definition cons_cnf_cell (l: list Z): cnf_list_cell :=
-  CNFCell 3 l.
+(* Definition  (l: list Z): cnf_list_cell :=
+  CNFCell 3 l. *)
 
 Notation "x <>? y" := (negb (Z.eqb x y)) (at level 70).
 Notation "x ==? y" := (Z.eq_dec x y) (at level 70).
 
+Import smt_lang_enums1.
+
 (* p3 <-> (p1 op p2) to cnf *)
-Definition iff2cnf (p1 p2 p3: Z) (op: Z): cnf_list :=
+Definition iff2cnf_binary (p1 p2 p3: Z) (op: SmtPropBop): cnf_list :=
   match op with
-    | 0 => let c1 := cons_cnf_cell [p1; -p3] in
-            let c2 := cons_cnf_cell [p2; -p3] in
-              let c3 := if (p1 ==? p2) then cons_cnf_cell [-p1; p3] else cons_cnf_cell [-p1; -p2; p3] in
+    | SMTPROP_AND => let c1 := [p1; -p3] in
+            let c2 := [p2; -p3] in
+              let c3 := if (p1 ==? p2) then [-p1; p3] else [-p1; -p2; p3] in
                 c1 :: c2 :: c3 :: nil
-    | 1 => let c1 := cons_cnf_cell [-p1; p3] in
-            let c2 := cons_cnf_cell [-p2; p3] in
-              let c3 := if (p1 ==? p2) then cons_cnf_cell [p1; -p3] else cons_cnf_cell [p1; p2; -p3] in
+    | SMTPROP_OR => let c1 := [-p1; p3] in
+            let c2 := [-p2; p3] in
+              let c3 := if (p1 ==? p2) then [p1; -p3] else [p1; p2; -p3] in
                 c1 :: c2 :: c3 :: nil
-    | 2 => if (p1 ==? p2) then
-              (cons_cnf_cell (p3 :: nil)) :: nil
+    | SMTPROP_IMPLY => if (p1 ==? p2) then
+              ( (p3 :: nil)) :: nil
            else
-            let c1 := cons_cnf_cell [p1; p3] in
-              let c2 := cons_cnf_cell [-p2; p3] in
-                let c3 := cons_cnf_cell [-p1; p2; -p3] in
+            let c1 := [p1; p3] in
+              let c2 := [-p2; p3] in
+                let c3 := [-p1; p2; -p3] in
                   c1 :: c2 :: c3 :: nil
 
-    | 3 => if (p1 ==? p2) then
-            (cons_cnf_cell (p3 :: nil)) :: nil
+    | SMTPROP_IFF => if (p1 ==? p2) then
+            ( (p3 :: nil)) :: nil
            else
-            let c1 := cons_cnf_cell [p1; p2; p3] in
-              let c2 := cons_cnf_cell [-p1; -p2; p3] in
-                let c3 := cons_cnf_cell [p1; -p2; -p3] in
-                  let c4 := cons_cnf_cell [-p1; p2; -p3] in
+            let c1 := [p1; p2; p3] in
+              let c2 := [-p1; -p2; p3] in
+                let c3 := [p1; -p2; -p3] in
+                  let c4 := [-p1; p2; -p3] in
                     c1 :: c2 :: c3 :: c4 :: nil
-
-    | 4 => let c1 := cons_cnf_cell [p2; p3] in
-            let c2 := cons_cnf_cell [-p2; -p3] in
-              c1 :: c2 :: nil
-    | _ => nil
   end.
 
-Definition iff2cnf_length (p1 p2 p3: Z) (op: Z): Z :=
+Definition iff2cnf_length_binary (p1 p2 p3: Z) (op: SmtPropBop): Z :=
   match op with
-    | 0 => 3%Z
-    | 1 => 3%Z
-    | 2 => if (p1 ==? p2) then 1%Z else 3%Z
-    | 3 => if (p1 ==? p2) then 1%Z else 4%Z
-    | 4 => 2%Z
-    | _ => 0%Z
+    | SMTPROP_AND => 3%Z
+    | SMTPROP_OR => 3%Z
+    | SMTPROP_IMPLY => if (p1 ==? p2) then 1%Z else 3%Z
+    | SMTPROP_IFF => if (p1 ==? p2) then 1%Z else 4%Z
   end.
+
+Definition iff2cnf_unary (p2 p3: Z) (op: SmtPropUop): cnf_list :=
+  let c1 := [p2; p3] in
+    let c2 := [-p2; -p3] in
+      c1 :: c2 :: nil.
+
   (* 0 => AND
      1 => OR
      2 => IMPLY
